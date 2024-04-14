d1c4d52192d0ceeb9f2446369b9dabd5bb730a8a
Franco Valledor <franco.valledor@bluealba.com>
2024-04-14 19:39:12 -0300

    disable CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,10 +3,12 @@ import express from "express";
 import redisClient from "./redisClient";
 import { router as mandalorianRouter } from "./mandalorian/router";
 import { errorHandler } from "./errorHandler";
+import { disableCorsMiddleware } from "./disableCorsMiddleware";
 
 const SERVER_PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(disableCorsMiddleware);
 
 app.use("/the-mandalorian", mandalorianRouter);
 app.use(errorHandler);
