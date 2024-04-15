5a80be330e2fec865e5adc3470cbb56cf7d568cc
Franco Valledor <franco.valledor@bluealba.com>
2024-04-14 23:44:33 -0300

    add router

--- mandalorian-ui/src/App.tsx
+++ mandalorian-ui/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>The Mandalorian</h1>
+      <Layout />
+      <Routes>
+        <Route path="/" element={<div>main</div>}></Route>
+        <Route path="about" element={<div>about</div>} />
+        <Route path="dashboard" element={<div>dashboard</div>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const Layout: React.FC = () => {
+  return (
+    <ul>
+      <li>
+        <Link to={"/"}>HOME</Link>
+      </li>
+      <li>
+        <Link to={"about"}>ABOUT</Link>
+      </li>
+      <li>
+        <Link to={"dashboard"}>DASHBOARD</Link>
+      </li>
+    </ul>
+  );
+};
